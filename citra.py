9457fa99700c2e75bf17e25035bb561b07019965
unknown <fathur.risqo@gmail.com>
2025-02-04 09:54:03 +0700

    Refactoring Code

--- citra.py
+++ citra.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 
-image_path = 'frame_1.png'
+image_path = 'frame_2.png'
 citra = cv.imread(image_path)
 
 # Ambil warna RGB dari gambar
@@ -15,39 +15,32 @@ def getColors(image):
 def grayScale(image):
     rows = len(image)
     cols = len(image[0])
-
     b, g, r = getColors(image)
-    get_grayscale = np.zeros((rows,cols))
+    grayscale = np.zeros((rows,cols))
 
     for i in range(rows):
         for j in range(cols):
-            get_grayscale[i,j] = 0.299 * r[i,j] + 0.587 * g[i,j] + 0.114 * b[i,j]
+            grayscale[i,j] = 0.299 * r[i,j] + 0.587 * g[i,j] + 0.114 * b[i,j]
 
-    return get_grayscale.astype(np.uint8)
+    return grayscale.astype(np.uint8)
 
 
 # ubah gambar Grayscale menjadi Black and White
 def blackWhiteFilter(image, treshold=128):
     rows = len(image)
     cols = len(image[0])
-
     grayscale = grayScale(image)    
-    get_blackwhite = np.zeros((rows, cols))
+    blackwhite = np.zeros((rows, cols))
 
     for i in range(rows):
         for j in range(cols):
-            if grayscale[i,j] >= treshold:
-                get_blackwhite[i,j] = 255
-            else:
-                get_blackwhite[i,j] = 0
+            blackwhite[i,j] = 255 if grayscale[i,j] >= treshold else 0
     
-    return get_blackwhite.astype(np.uint8)
+    return blackwhite.astype(np.uint8)
 
 
 citra_grayscale = grayScale(citra)
-citra_blackwhite = blackWhiteFilter(citra)
+citra_blackwhite = blackWhiteFilter(citra, 98)
 
-cv.imshow("Soseo Black and White", citra_blackwhite)
-cv.imwrite("frame1_GrayScale.png", citra_grayscale)
-cv.imwrite("frame1_BlackWhite.png", citra_blackwhite)
+cv.imshow("Grayscale", citra_grayscale)
 cv.waitKey()
